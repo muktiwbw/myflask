dd89e0815226deecab61880c5af5e6eaa267cc89
Mukti Wibowo <muktiw7@gmail.com>
2020-06-20 16:46:29 +0700

    Rename config folder to setting

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-from app.config import Config
+from app.setting.config import Config
 
 # ==================================================
 # Initialize extension instances
--- app/setting/model.py
+++ app/setting/model.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -8,6 +9,7 @@ class User(db.Model):
     name = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(60), nullable=False)
     avatar = db.Column(db.String(100), nullable=False, default='default.jpg')
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"('id': {self.id}, 'username': {self.username}, 'email': {self.email},'name': {self.name},'avatar': {self.avatar})"
+        return f"('id': {self.id}, 'username': {self.username}, 'email': {self.email},'name': {self.name},'avatar': {self.avatar}, created_at: {self.created_at})"
