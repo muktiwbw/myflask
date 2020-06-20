dee9bbe0c9794278805a9f4fcef7ba12b86ee06b
Mukti Wibowo <muktiw7@gmail.com>
2020-06-20 13:41:27 +0700

    Added initial User model

--- app/config/model.py
+++ app/config/model.py
@@ -1,3 +1,13 @@
 from app import db
 
- 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    avatar = db.Column(db.String(100), nullable=False, default='default.jpg')
+
+    def __repr__(self):
+        return f"('id': {self.id}, 'username': {self.username}, 'email': {self.email},'name': {self.name},'avatar': {self.avatar})"
