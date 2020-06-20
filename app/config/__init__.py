77fa532252530ddfeca1a51f6f472b8651f69f23
Mukti Wibowo <muktiw7@gmail.com>
2020-06-20 11:47:52 +0700

    Added database host port

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -14,7 +14,7 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
 
     # SQL Alchemy
-    SQLALCHEMY_DATABASE_URI = f"mysql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_DATABASE')}"
+    SQLALCHEMY_DATABASE_URI = f"mysql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_DATABASE')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
     # JWT
